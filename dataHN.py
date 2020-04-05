d3ab872d9eabbd4c407c9afe7e6babde424b3f41
alb2m2 <38852275+alb2m2@users.noreply.github.com>
2020-04-05 00:39:44 -0400

    Update dataHN.py

--- dataHN.py
+++ dataHN.py
@@ -30,8 +30,4 @@ Honduras['Country/Region']=descriptions
 Honduras.rename(columns={'Country/Region':'Status'})
 
 #Saving data into a local file 
-death_HN_df.to_csv('/home/alb2m2/Documents/muertosHN.csv')
-confirmed_HN_df.to_csv('/home/alb2m2/Documents/confirmadosHN.csv')
-recovered_HN_df.to_csv('/home/alb2m2/Documents/recuperadosHN.csv')
-country_HN_df.to_csv('/home/alb2m2/Documents/paisesHN.csv')
 Honduras.to_csv('/home/alb2m2/Documents/HondurasResumen.csv')
